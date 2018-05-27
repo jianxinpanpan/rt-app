2b60f3b7a2eb99eb8e3517945e0b4a37d11f8335
jianxinpanpan <jianxin_pan@163.com>
2018-05-27 06:51:19 -0700

    Revert "fix json-c compile error"
    This reverts commit e2c5e3187f845940f7f0be2404e6dcf839885c68.

--- json-c/mk_arm.sh
+++ json-c/mk_arm.sh
@@ -2,4 +2,4 @@ export ac_cv_func_malloc_0_nonnull=yes
 export ac_cv_func_realloc_0_nonnull=yes
 ./autogen.sh
 ./configure --host=arm-linux-gnueabihf  --disable-shared --enable-static
-make -j4
+make
--- mk_arm.sh
+++ mk_arm.sh
@@ -1,5 +1,5 @@
 #cd json-c and sh mk_arm.sh
 export ac_cv_lib_json_c_json_object_from_file=yes
 ./autogen.sh
-./configure --host=arm-linux-gnueabihf LDFLAGS="--static -L./../json-c/." CFLAGS="-I./.." --with-deadline
+./configure --host=arm-linux-gnueabihf LDFLAGS="--static -L./../json-c/." CFLAGS="-I./.." --with-deadline --prefix=./__install 
 make
